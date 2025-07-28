c7445389005b7c330850d758b54a6cd042c650b7
AJ <richie.okoro@gmail.com>
2025-07-28 06:38:33 +0100

    implemented startservice func in app.js

--- api/app.js
+++ api/app.js
@@ -5,25 +5,35 @@ const path = process.env['SSM_PARAMETER_PATH'] || '/devconzero/env/';
 
 if (process.env.NODE_ENV === 'production') {
   if (process.env.CLOUD === 'aws') {
-    await getParameters(path)
+    getParameters(path).then(() => {
+      console.log("Parameters loaded from AWS SSM");
+      startservice();
+    })
   }
+} else {
+  console.log("No AWS SSM parameters to load, starting service...");
+  startservice();
 }
 
-const app = express();
+async function startservice() {
+  console.log("Starting service...");
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json());
-
-app.get('/api', (req, res) => {
-  res.json({ "message": "/api endpoint", "status": "true" })
-});
-
-app.get('/api/test', (req, res) => {
-  res.json({ "message": "Test endpoint", "status": "true", "test": process.env.TEST || 'NOT_FOUND' });
-});
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`API server is running on port ${PORT}`);
-});
+  const app = express();
+  
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json());
+  
+  app.get('/api', (req, res) => {
+    res.json({ "message": "/api endpoint", "status": "true" })
+  });
+  
+  app.get('/api/test', (req, res) => {
+    res.json({ "message": "Test endpoint", "status": "true", "test": process.env.TEST || 'NOT_FOUND' });
+  });
+  
+  const PORT = process.env.PORT || 4000;
+  
+  app.listen(PORT, () => {
+    console.log(`API server is running on port ${PORT}`);
+  });
+}
